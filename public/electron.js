7069f617b32994497d9fe6f381f9daeb3fc8d3e2
karmelyoei <karmel.salah@gmail.com>
2020-12-12 17:23:02 +0200

    readme

---
+++ public/electron.js
@@ -0,0 +1,41 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true,
+      worldSafeExecuteJavaScript:true,
+      contextIsolation:true
+    },
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+  mainWindow.removeMenu();
+  mainWindow.on('closed', () => (mainWindow = null));
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
